a2a0b64bfda05d0e5995e0f7baa3292edc8975fd
Franky Rodriguez <frankyan2001@gmail.com>
2024-10-11 18:05:30 -0400

    Created Change Scene method in Scene controller

--- SceneController.cs
+++ SceneController.cs
@@ -8,6 +8,15 @@ public class ControladorEscena : MonoBehaviour
     public float duracionEscena = 5f; // Duración en segundos
     public string nombreSiguienteEscena; // Nombre de la siguiente escena
 
+    void Start()
+    {
+        // Inicia el temporizador para cambiar a la siguiente escena después de la duración especificada
+        Invoke("CambiarEscena", duracionEscena);
+    }
 
-    
+    void CambiarEscena()
+    {
+        // Cambia a la siguiente escena
+        SceneManager.LoadScene(nombreSiguienteEscena);
+    }
 }
