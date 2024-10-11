5dbc1a755d72b786272e66f2d6d60bad410ba3c4
Franky Rodriguez <frankyan2001@gmail.com>
2024-10-11 18:04:16 -0400

    Created Scene controller

---
+++ SceneController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControladorEscena : MonoBehaviour
+{
+    public float duracionEscena = 5f; // Duración en segundos
+    public string nombreSiguienteEscena; // Nombre de la siguiente escena
+
+
+    
+}
