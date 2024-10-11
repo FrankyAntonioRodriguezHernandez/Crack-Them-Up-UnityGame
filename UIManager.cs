a189b7ba5d91296d808b72bd1107aaf9999e422d
Franky Rodriguez <frankyan2001@gmail.com>
2024-10-11 18:09:14 -0400

    Created Start method for increase laughs in UiManager Script

--- UIManager.cs
+++ UIManager.cs
@@ -4,5 +4,20 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 public class UiManager : MonoBehaviour{
+
+    [SerializeField] private List <GameObject> listaVidas;
+    [SerializeField] private Sprite mascaraRota;
+    [SerializeField] private Sprite mascara;
+    public int cantidadRisas;
+    [SerializeField] private TMP_Text textoRisas;
+
+    private void Start()
+    {
+        // Inicia la invocación repetida para aumentar las risas cada 15 segundos
+        InvokeRepeating("AumentarRisasPeriodicamente", 0f, 15f);
+    }
     
+
+
+
 }
