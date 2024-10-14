64c5a3df1102abbff95df373971291205bd94d82
Franky Rodriguez <frankyan2001@gmail.com>
2024-10-13 23:43:59 -0400

    Creating Start method addign Rigidbody2d and Animator in CharacterMovement Script

--- CharacterMovement.cs
+++ CharacterMovement.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
 
-
-
-
+    [SerializeField] private ParticleSystem particulas;
+    [SerializeField] private float velocidad = 5f;
+    [SerializeField] private UiManager uiManager;
+    private Rigidbody2D rb;
+    [SerializeField] private int vidaPersonaje = 5;
+    private Animator playerAnimator;
+    private Vector2 direccion;
+    [SerializeField] private GameObject menuDerrota;
+    private bool jugadorHaMuerto =false ;
 
 
 private void void Start()
 {
-    
+    rb = GetComponent<Rigidbody2D>();
+    playerAnimator = GetComponent<Animator>();
 }
 
 private void void Update()
