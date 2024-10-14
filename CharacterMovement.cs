35808cc3c628b51b98e7cdcae03b5639dec7461e
Franky Rodriguez <frankyan2001@gmail.com>
2024-10-13 23:40:42 -0400

    Creating CharacterMovement Script

---
+++ CharacterMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+
+
+
+
+
+private void void Start()
+{
+    
+}
+
+private void void Update()
+{
+    
+}
+
+
+
+
+
+}
