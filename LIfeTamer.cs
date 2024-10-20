caa26c62320d01e76e7b7327b3c1fdacc7dfea87
Franky Rodriguez <frankyan2001@gmail.com>
2024-10-20 10:42:39 -0400

    Created hurt method in LifeTamer Script

--- LIfeTamer.cs
+++ LIfeTamer.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class VidaTamer : MonoBehaviour
 {
+    private float vidaTamer=2;
+    private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("ArmaPersonaje"))
+            {
+                CausarHerida();
+            }
+        }
 
+    private void CausarHerida()
+        {
+            if (vidaTamer > 0)
+            {
+                vidaTamer--;
+           
+            
+        }
+    }
 }
