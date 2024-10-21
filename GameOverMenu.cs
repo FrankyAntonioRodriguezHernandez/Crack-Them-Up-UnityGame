ee5893afa2039d0f48f4d8fdcb75fb2fc368f132
Franky Rodriguez <frankyan2001@gmail.com>
2024-10-21 10:32:02 -0400

    finished GameOverMenu Script

--- GameOverMenu.cs
+++ GameOverMenu.cs
@@ -6,7 +6,6 @@ using UnityEngine.SceneManagement;
 public class GameOverMenu : MonoBehaviour
 {
     public GameObject gameOverPanel;
-    public string nombreSiguienteEscena;
 
     void Start()
     {
