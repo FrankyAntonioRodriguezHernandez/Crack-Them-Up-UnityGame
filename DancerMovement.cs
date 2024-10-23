b52b57b92ff9a93625286234a1b8b716cabf9f55
Franky Rodriguez <frankyan2001@gmail.com>
2024-10-23 19:45:11 -0400

    Implemented Start method in DancerMovement Script

--- DancerMovement.cs
+++ DancerMovement.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class DancerMovement : MonoBehaviour
 {
-    
+    private Transform objetivo;
+    [SerializeField] private float velocidad = 5f;
+    [SerializeField] private float rangoDeAtaque = 1.5f;
+    private Animator dancerAnimator;
+    private float vidaDancer =2;
+
+    void Start()
+    {
+         objetivo = GameObject.Find("Personaje").transform;
+        dancerAnimator = GetComponent<Animator>();
+    }
+
+
+
+
+
+
 }
