bed0e1d77be3f79c9395184c1be4be9dd03557c9
Franky Rodriguez <frankyan2001@gmail.com>
2024-10-24 11:58:29 -0400

    Finished BeastController Script

--- BeastController.cs
+++ BeastController.cs
@@ -23,31 +23,18 @@ public class BeastController : MonoBehaviour
     {
         if (objetivo != null)
         {
-            // Obtén la dirección hacia el objetivo
             Vector2 direccion = (objetivo.position - transform.position).normalized;
-
-            // Calcula la distancia al objetivo
             float distanciaAlObjetivo = Vector2.Distance(transform.position, objetivo.position);
-
-            // Si está dentro del rango de ataque, ataca
             if (distanciaAlObjetivo <= rangoDeAtaque)
             {
                 Atacar();
             }
             else
             {
-                // Mueve la bestia hacia el objetivo
                 transform.position = Vector2.MoveTowards(transform.position, objetivo.position, velocidad * Time.deltaTime);
-
-                // Configura la rotación de la bestia
                 transform.right = direccion;
-
-                // Restaura el booleano cuando no está atacando
                 atacando = false;
-                // Configura el booleano en el Animator
                 bestiaAnimator.SetBool("Atacando", atacando);
-
-                // Desactiva el colisionador cuando no está atacando
                 boxCollider.enabled = false;
             }
         }
