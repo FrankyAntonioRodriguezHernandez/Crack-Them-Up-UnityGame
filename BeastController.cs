5498833ce1825a4484f4f66b820856234b060f09
Franky Rodriguez <frankyan2001@gmail.com>
2024-10-24 11:54:26 -0400

    Implemented Start method in BeastController Script

--- BeastController.cs
+++ BeastController.cs
@@ -1,6 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BeastController : MonoBehaviour
 {
+    private Transform objetivo;
+    [SerializeField] private float velocidad = 5f;
+    [SerializeField] private float rangoDeAtaque = 1.5f;
+    private bool atacando = false;
+    private Animator bestiaAnimator;
+    private BoxCollider2D boxCollider;
+
+    void Start()
+    {
+         objetivo = GameObject.Find("Personaje").transform;
+        bestiaAnimator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        boxCollider.enabled = false;
+    }
+
+
+
+
     
 }
