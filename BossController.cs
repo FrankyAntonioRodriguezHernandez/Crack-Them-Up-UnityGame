ecc3def4dfbadc258f1772761d81519be0aac129
Franky Rodriguez <frankyan2001@gmail.com>
2024-10-27 11:26:07 -0400

    Implemented Start method in BossController Script

--- BossController.cs
+++ BossController.cs
@@ -3,5 +3,15 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
-    
+    private Transform objetivo; // El objetivo a atacar (puedes configurarlo en el Inspector)
+    private float rangoDeAtaque = 1.5f; // Rango de ataque del bot
+
+    private Animator botAnimator;
+
+    void Start()
+    {
+        botAnimator = GetComponent<Animator>();
+        // Encuentra el objetivo por su nombre (asegúrate de que el objetivo tenga el nombre correcto)
+        objetivo = GameObject.Find("Personaje").transform;
+    }
 }
